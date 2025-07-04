6de608ef9d67ff76d3afb188d306bb02dc41ecd8
Godsway Sackey <geesackeyfio@gmail.com>
2025-07-04 14:07:22 +0200

    Cleanup: keep only docs/, .git, and README.md in root for GitHub Pages deployment

--- pwa.js
+++
@@ -1,7 +0,0 @@
-export function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-      .then(() => console.log('Service worker registered!'))
-      .catch(err => console.log('SW registration failed:', err));
-  }
-}
